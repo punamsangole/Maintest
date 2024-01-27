667cb518c540862bd6b54953bd4434227df485d6
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-27 11:46:29 +0530

    added

--- maintest/src/maintest/Sample.java
+++ maintest/src/maintest/Sample.java
@@ -4,6 +4,12 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("loacl respo");
 		System.out.println("remote repository master repository");
+		System.out.println("loacl respo");
+		System.out.println("remote repository master repository");
+		System.out.println("loacl respo");
+		System.out.println("remote repository master repository");
+		
+		
 		
 	}
 
