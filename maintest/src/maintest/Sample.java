78e508ef2c05885f7a75905bb2044511ec996535
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-27 10:19:05 +0530

    added line by remote

--- maintest/src/maintest/Sample.java
+++ maintest/src/maintest/Sample.java
@@ -3,6 +3,8 @@ package maintest;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("loacl respo");
+		System.out.println("remote repository master repository");
+		
 	}
 
 }
